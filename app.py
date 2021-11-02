8fec729512d1caa33216ba70e3e92a21ad0608d7
Abhijeet Sinha <abhijeetaman007@gmail.com>
2021-11-03 01:38:04 +0530

    SMS and Email api testing

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask,render_template,url_for,request,redirect,jsonify
 from flask_sqlalchemy import SQLAlchemy
 import models
+import requests
+from flask_mail import Mail, Message
+import smtplib
 
 app = Flask(__name__)
+
+#DB Config
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
 db = SQLAlchemy(app) 
+
+
+mail = Mail(app)
+
+#Mailer Config
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'birthdaywishingbot@gmail.com'
+app.config['MAIL_PASSWORD'] = 'approject2021'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+    
+
 
 # Home page route
 @app.route('/')
@@ -37,6 +56,45 @@ def test():
         return jsonify({"Response":"Get Request Called"})
 
     # return render_template('index.html',tasks=tasks)
+
+
+
+
+#SMS sending route
+@app.route('/sendsms',methods=['POST','GET'])
+def sendSMS():
+    print("Sending SMS")
+    url = "https://www.fast2sms.com/dev/bulkV2"
+    number = "7091788126"
+    message = "Happy Birthday"
+    querystring = {"authorization":"YyvBSlen5O2ALDk4IQUVzsXHCqMRK7i9aNJwp0x3ud1G6fjTbchiQXjoG2RWI8mTJKODE4cA7baFtx9M","message":message,"language":"english","route":"q","numbers":number}
+
+    headers = {
+        'cache-control': "no-cache"
+    }
+
+    resp = requests.request("GET", url, headers=headers, params=querystring)
+
+    print(resp.text)
+
+    return jsonify({"response":resp.text})
+
+
+
+
+#Email sending route
+@app.route('/sendemail',methods=['POST','GET'])
+def sendEmail():
+    print("Sending Email")    
+    msg = Message(
+                    'Hello testing mailer',
+                    sender ='birthdaywishingbot@gmail.com',
+                    recipients = ['abhijeetsinha1503@gmail.com']
+                )
+    msg.body = 'Hello Flask message sent from Flask-Mail'
+    mail.send(msg)
+    
+    return jsonify({"response":"Mail sent"})
 
 if __name__ == "__main__":
     app.run(debug=True)
